8a236dc148997799e40f7ed4414c6ca1d35eef48
Anil Singh Rasaputhra <anil.rasaputhra@computechcorp.com>
2024-10-31 07:35:12 +0530

    Implemented Singleton

---
+++ ConsoleApp1/UploadServices.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class UploadServices
+    {
+        private static UploadServices instance;
+        private static int coutOfInstance = 0;
+        private static List<string> menu;
+        private UploadServices() 
+        {
+            menu = new List<string>()
+            {
+                "Vada",
+                "Idly",
+                "Puri"
+            };
+        }
+
+        
+
+        public static UploadServices Intance(int a)
+        {
+            if (instance == null)
+            {
+                UploadServices _instance = new UploadServices ();
+                instance = _instance;
+                coutOfInstance = a;
+            }
+            return instance;
+        }
+    }
+}
