d2e14a070209b40d367440c2cd0d51095d2af462
Rasaputhra Anil Singh <anilsinghr2235@gmail.com>
2024-11-15 08:30:46 +0530

    Added Comments

--- ConsoleApp1/CodeConcepts.cs
+++ ConsoleApp1/CodeConcepts.cs
@@ -17,7 +17,7 @@ namespace ConsoleApp1
             {
                 { 1,2,3},{ 5,6,7}
             };
-
+            //looping to array
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
